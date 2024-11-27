82dc0fef30c4531622dd9d0dd1dbe4a3e881d270
tacherounte mohammed <mohadavid25@gmail.com>
2024-11-27 12:01:50 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,62 +1,37 @@
 const express = require('express');
-
 const app = express();
-const Note  = require('./models/note')
+const Note = require('./models/note')
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+const noteRouter = require('./routes/note_route');
+const mongoDbAtlasPath = 'mongodb+srv://admin:fcb1234567890@affiliate.d2hwz.mongodb.net/?retryWrites=true&w=majority&appName=affiliate';
+//const mongoDbPath = 'mongodb://localhost:27017/affiliate_db1'
 
-const mongoDbPath = 'mongodb+srv://admin:fcb1234567890@affiliate.d2hwz.mongodb.net/?retryWrites=true&w=majority&appName=affiliate';
-mongoose.connect(mongoDbPath).then(() => {
+
+
+mongoose.connect(mongoDbAtlasPath).then(() => {
     console.log('connected successfully elhamdolilah');
-    app.get('/',(req,res)=>{
-        const responce = {statuscode:res.statusCode,message:'Api Works'};
-        res.json(responce);
-
-    })
-    //////////
-    // app.get('/notes/list', async (req, res) => {
-    //     const notes = await Note.find();
-    //     console.log(notes)
-    //     res.json(notes);
-       
-    // });
-    ///////////////////
-    const noteRouter = require('./routes/note_route')
 
     app.use('/note', noteRouter);
+    app.get('/', (req, res) => {
+        const responce = { statuscode: res.statusCode, message: 'Api Works' };
+        res.json(responce);
+    });
+
 
 
 }).catch((error) => {
     console.log('error of connect', error);
 });
-const port = process.env.port||3000;
+const port = process.env.port || 3000;
 
 
 app.listen(port, () => {
     console.log('successfullly connected at port' + `${port}`)
+
 });
 
 
-// app.post('/api/add_person',(req,res)=>{
-
-//     console.log('result',req.body);
-// const pdata = {
-
-//     'id':personeData.length+1,
-//     'name':req.body.name,
-//     'phone':req.body.phone,
-//     'age':req.body.age,
-// }
-// personeData.push(pdata),
-// console.log('final result',pdata),
-
-// res.send('new person')
-
-// // res.statusCode(200).send({
-// //     "Status_code" : 200,
-// //     "Message":"data is added successfully",
-// //     "person":pdata,
-// // });
-
-// })
 
 
--- models/note.js
+++ models/note.js
@@ -27,4 +27,4 @@ const noteSchema = mongoose.Schema({
 
 
 
-module.exports =  mongoose.model('Note',noteSchema)
+module.exports =  mongoose.model('note',noteSchema)
